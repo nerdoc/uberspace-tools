ea330f388e4fc5ff72df7943c5d8d58daa8af6aa
Christian González <christian.gonzalez@nerdocs.at>
2016-02-04 01:50:10 +0100

    added / removed code style whitespace

--- install-mailfilter.sh
+++ install-mailfilter.sh
@@ -20,7 +20,7 @@
 
 this="uberspace-mail-tools"
 
-#install DSPAM
+# install DSPAM
 echo "Installing DSPAM service..."
 test -d ~/service || uberspace-setup-svscan
 test -d ~/etc/dspam-learn && rm -rf ~/etc/dspam-learn
@@ -28,14 +28,12 @@ runwhen-conf ~/etc/dspam-learn ~/$this/dspam-learn > /dev/null
 sed -i -e "s/^RUNWHEN=.*/RUNWHEN=\",M=`awk 'BEGIN { srand(); printf("%d\n",rand()*60) }'`\"/" ~/etc/dspam-learn/run
 ln -sf ~/etc/dspam-learn ~/service
 
-
 # install DSPAM cleanup service
 echo "Installing DSPAM cleanup service..."
 test -d ~/etc/dspam_clean_hashdb && rm -rf ~/etc/dspam_clean_hashdb
 runwhen-conf ~/etc/dspam_clean_hashdb "/usr/local/bin/dspam_clean_hashdb" > /dev/null
 sed -i -e "s/^RUNWHEN=.*/RUNWHEN=\",H=`awk 'BEGIN { srand(); printf("%d\n",rand()*24) }'`\"/" ~/etc/dspam_clean_hashdb/run
 ln -sf ~/etc/dspam_clean_hashdb ~/service
-
 
 # install mailfilter file
 cp ~/$this/mailfilter.tpl ~/.mailfilter
