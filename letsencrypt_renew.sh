17d09f0942a4ba37f9d30fa43a7bd62276a34982
Christian González <christian.gonzalez@nerdocs.at>
2016-03-30 18:29:43 +0200

    only use one domain and check for valid 'domains' string

--- letsencrypt_renew.sh
+++ letsencrypt_renew.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 valid_days=10
-cert_dir=/home/$USER/.config/letsencrypt/live
+letsencrypt_dir=/home/$USER/.config/letsencrypt
+cert_dir=${letsencrypt_dir}/live
 cert_file='cert.pem'
 domain=$(grep -e "[ \t]*domains.*" ~/.config/letsencrypt/cli.ini| sed "s/ //g" |cut -d "=" -f2 | cut -d "," -f1)
 
+if [ "$domain" == "" ]; then
+  echo "the 'domains' variable in ${letsencrypt_dir} seems to be malformed or missing."
+  echo "Please check this file. (Hint: have you run uberspace-letsencrypt already?)"
+  exit 1
+fi
 
 # find all certificates
 export cert="$(find $cert_dir/$domain/ -iname $cert_file | sort -k1)"
@@ -13,6 +19,6 @@ openssl x509 -checkend $(( $valid_days * 86400 )) -in "$cert" > /dev/null
 
 # renew certificates if they are less than only 10 more days valid
 if [ $? != 0 ]; then
-   letsencrypt certonly
+   letsencrypt certonly -d $domain
    uberspace-prepare-certificate -k $cert_dir/$domain/privkey.pem -c $cert_dir/$domain/$cert_file
 fi
